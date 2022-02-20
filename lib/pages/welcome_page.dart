3ef1fb0620881373f5b73c9bc7c33a26904aa9fb
Jovan Penezic <jovan.penezic@gmail.com>
2022-02-20 11:22:01 +0100

    Slider implemented

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -44,13 +44,14 @@ class _WelcomePageState extends State<WelcomePage> {
               child: Container(
                 margin: const EdgeInsets.only(top:150, left: 20, right: 20),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         AppLargeText(text: "Trips"),
                         AppText(text: "Mountain", textSize: 30),
-                        SizedBox(height: 20,),
+                        const SizedBox(height: 20),
                         Container(
                           width: 250,
                           child: AppText(
@@ -59,9 +60,22 @@ class _WelcomePageState extends State<WelcomePage> {
                             textSize: 14,
                           ),
                         ),
-                        SizedBox(height: 20,),
+                        const SizedBox(height: 20),
                         ResponsiveButton(width: 120)
                       ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDots) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 2),
+                          width: 8,
+                          height: index==indexDots?25:8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index==indexDots?AppColors.mainColor:AppColors.mainColor.withOpacity(0.3)
+                          ),
+                        );
+                      }),
                     )
                   ],
                 )
